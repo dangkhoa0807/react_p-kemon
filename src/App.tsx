b390d11f6b44855a213dec2ed190372b08ad3e3b
dangkhoa0807 <khoatran2b@gmail.com>
2024-05-15 20:53:10 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+import './App.css';
+import axios from 'axios';
+import PokemonCollection from './components/PokemonCollection';
+import { Pokemon } from './interface';
+
+
+interface Pokemons{
+  name: string;
+  url: string;
+}
+
+export interface Detail {
+  id: number;
+  isOpened: boolean;
+}
+
+const App: React.FC =()=>{
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  const [nextUrl, setNextUrl] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(true);
+  const [viewDetail, setDetail] = useState<Detail>({
+    id: 0,
+    isOpened: false,
+  });
+  useEffect(()=>{
+    
+    const getpoke= async()=>{
+      const res= await axios.get("https://pokeapi.co/api/v2/pokemon?limit=20&offset=20")
+
+      setNextUrl(res.data.next);
+      const pokemonData: Pokemon[] = [];
+      for (const pokemon of res.data.results) {
+        const poke = await axios.get(
+          `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+        );
+        pokemonData.push(poke.data);
+        setLoading(false);
+      }
+
+      setPokemons(pokemonData);
+    }
+    getpoke();
+  },[]);
+
+  const morePokemon=async ()=>{
+    setLoading(true);
+    const res= await axios.get(nextUrl);
+    setNextUrl(res.data.next);
+    res.data.results.forEach(async (pokemon:Pokemons) => {
+      const poke= await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+      
+      setLoading(false);
+      setPokemons((p)=> [...p,poke.data])
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="pokemon-header">
+          Pokemon
+        </div>
+          <PokemonCollection pokemons={pokemons} viewDetail={viewDetail} setDetail={setDetail}/>
+          {!viewDetail.isOpened && (
+          <div className="btn">
+            <button onClick={morePokemon}>
+              {loading ? "Loading..." : "Load more"}{" "}
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
